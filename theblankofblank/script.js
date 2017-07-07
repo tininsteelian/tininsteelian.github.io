9eb9e0b0fb8bb49fe513690fa6081e31537e0076
tininsteelian <tininsteelian@gmail.com>
2017-07-06 23:33:10 -0400

    the blank of blank
    99.999% chance this doesn't work

---
+++ theblankofblank/script.js
@@ -0,0 +1,37 @@
+var numCorp = Math.floor(Math.random() * 322);
+var numNoun = Math.floor(Math.random() * 4553);
+console.log(numCorp);
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+var corpsFile = readTextFile('corporations.txt');
+var nounsFile = readTextFile('nouns.txt');
+
+var corpsArray = corpsFile.split('\n');
+console.log(corpsArray);
+
+var nounsArray = nounsFile.split('\n');
+console.log(nounsArray);
+
+var corp = corpsArray[numCorp];
+var noun = nounsArray[numNoun];
+
+var line = "The " + corp + " of " + noun + ".";
+console.log(line);
+document.getElementById("text").innerHTML = line;
