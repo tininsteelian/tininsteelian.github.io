6a5f8dd9aa359fbf249cf8d73ad344764fb3f0ee
tininsteelian <tininsteelian@gmail.com>
2019-02-23 16:11:52 -0500

    flip/flop

---
+++ games/flipflop/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1550956249,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/arrows-sheet0.png",
+		"images/arrows2-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"media/explosion21.m4a",
+		"media/explosion21.ogg",
+		"media/jump15.m4a",
+		"media/jump15.ogg",
+		"media/randomize59.m4a",
+		"media/randomize59.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
