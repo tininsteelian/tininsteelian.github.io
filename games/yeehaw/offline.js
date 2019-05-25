836f78f598786503694622c4214f1a80559eb710
tininsteelian <tininsteelian@gmail.com>
2019-05-25 17:15:54 -0400

    yee haw
    yeehaw

---
+++ games/yeehaw/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1558818896,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/yeehawperson-sheet0.png",
+		"images/yeehawperson-sheet1.png",
+		"images/yeehawhat-sheet0.png",
+		"images/pexelsphoto-sheet0.png",
+		"media/yeehawnopause.m4a",
+		"media/yeehawnopause.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
