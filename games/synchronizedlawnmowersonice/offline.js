73301626fc4973b4f1e0b9f245400114b834bf94
tininsteelian <tininsteelian@gmail.com>
2019-06-01 17:18:52 -0400

    Synchronized Lawn Mowers On Ice

---
+++ games/synchronizedlawnmowersonice/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1559423866,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"media/431477__kierankeegan__lawnmower-idle-close.m4a",
+		"media/431477__kierankeegan__lawnmower-idle-close.ogg",
+		"media/71739__audible-edge__chrysler-lhs-tire-squeal-04-04-25-2009.ogg",
+		"media/71739__audible-edge__chrysler-lhs-tire-squeal-04-04-25-2009.m4a",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
