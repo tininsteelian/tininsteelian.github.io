821d446677eea132412b226b933c40029566c86f
tininsteelian <tininsteelian@gmail.com>
2019-06-15 17:27:30 -0400

    mitosis bowling

---
+++ games/mitosisbowling/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1560633940,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/pin-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/wall-sheet0.png",
+		"media/401773__oztrum__plastic-cup-dropping.m4a",
+		"media/401773__oztrum__plastic-cup-dropping.ogg",
+		"media/65244__carbilicon__bowling.m4a",
+		"media/65244__carbilicon__bowling.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
