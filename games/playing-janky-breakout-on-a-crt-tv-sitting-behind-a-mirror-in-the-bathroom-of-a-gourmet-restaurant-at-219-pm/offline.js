318a4c3b18d23c47813a071e1df3194b1653dd09
tininsteelian <tininsteelian@gmail.com>
2019-06-22 17:05:17 -0400

    long title

---
+++ games/playing-janky-breakout-on-a-crt-tv-sitting-behind-a-mirror-in-the-bathroom-of-a-gourmet-restaurant-at-219-pm/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1561237365,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/bat-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/138406__cameronmusic__people-chatting-background.m4a",
+		"media/138406__cameronmusic__people-chatting-background.ogg",
+		"media/hit_hurt81.m4a",
+		"media/hit_hurt81.ogg",
+		"media/pickup_coin16.m4a",
+		"media/pickup_coin16.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
