2021682da7f96cc6bdcdd700169ad7ed6987b798
tininsteelian <tininsteelian@gmail.com>
2019-07-13 17:23:05 -0400

    cheese eater

---
+++ games/cheeseeater/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1563052876,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mouse-sheet0.png",
+		"images/mouseleg3-sheet0.png",
+		"images/mouseleg2-sheet0.png",
+		"images/mouseleg1-sheet0.png",
+		"images/cheese-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/436156__dersuperanton__aha-mouse-squirrel-cartoon.m4a",
+		"media/436156__dersuperanton__aha-mouse-squirrel-cartoon.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
