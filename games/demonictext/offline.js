a07a2e140482091c6cdd564a4c2c497c067ced47
tininsteelian <tininsteelian@gmail.com>
2019-08-03 17:24:36 -0400

    demonic text

---
+++ games/demonictext/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1564867244,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/d-sheet0.png",
+		"images/e-sheet0.png",
+		"images/m-sheet0.png",
+		"images/o-sheet0.png",
+		"images/n-sheet0.png",
+		"images/i-sheet0.png",
+		"images/c-sheet0.png",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/bell07.m4a",
+		"media/bell07.ogg",
+		"media/bell08.m4a",
+		"media/bell08.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
