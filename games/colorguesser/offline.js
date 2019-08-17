b7bd4b1f93970d36da26f50b235e612d974b76b6
tininsteelian <tininsteelian@gmail.com>
2019-08-17 17:16:46 -0400

    Color Guesser

---
+++ games/colorguesser/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1566076428,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/square-sheet0.png",
+		"images/square-sheet1.png",
+		"images/square-sheet2.png",
+		"images/background-sheet0.png",
+		"images/bar-sheet0.png",
+		"images/barb-sheet0.png",
+		"images/barg-sheet0.png",
+		"images/barr-sheet0.png",
+		"media/181859__timgormly__training-program-correct2.m4a",
+		"media/181859__timgormly__training-program-correct2.ogg",
+		"media/181858__timgormly__training-program-incorrect1.m4a",
+		"media/181858__timgormly__training-program-incorrect1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
