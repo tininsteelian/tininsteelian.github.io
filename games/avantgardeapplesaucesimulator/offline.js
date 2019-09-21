ce51e3cb33a8c9a6e5e673e2f293d28aca3782ce
tininsteelian <tininsteelian@gmail.com>
2019-09-21 17:30:19 -0400

    apple
    sauce

---
+++ games/avantgardeapplesaucesimulator/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1569101376,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/supermarket-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/shoppingcart-sheet0.png",
+		"images/apple-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/splatting.m4a",
+		"media/splatting.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
