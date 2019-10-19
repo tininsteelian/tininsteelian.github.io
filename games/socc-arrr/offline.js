5a8c167dfb708ffd867ffeb4345d283a608febfe
tininsteelian <tininsteelian@gmail.com>
2019-10-19 17:37:11 -0400

    socc-arrr
    arrrrrrrrrr

---
+++ games/socc-arrr/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1571520904,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"media/352102__inspectorj__splash-jumping-h.m4a",
+		"media/352102__inspectorj__splash-jumping-h.ogg",
+		"media/37158__volivieri__soccer-knee-01.m4a",
+		"media/37158__volivieri__soccer-knee-01.ogg",
+		"media/411492__inspectorj__jew-s-harp-single-a-h6-xy.m4a",
+		"media/411492__inspectorj__jew-s-harp-single-a-h6-xy.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
