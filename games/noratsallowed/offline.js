fc8a14d49e601f9591991047aa10a72a72bb2698
tininsteelian <tininsteelian@gmail.com>
2019-11-23 16:07:34 -0500

    rats
    rats

---
+++ games/noratsallowed/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1574543106,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/explode.m4a",
+		"media/explode.ogg",
+		"media/mouse.m4a",
+		"media/mouse.ogg",
+		"media/rat.m4a",
+		"media/rat.ogg",
+		"media/remove.m4a",
+		"media/remove.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
