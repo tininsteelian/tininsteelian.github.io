85b2d0e4dca40035c193e86fcd584f4e64158181
tininsteelian <tininsteelian@gmail.com>
2019-11-30 16:26:44 -0500

    donotdriveforwards
    DON'T

---
+++ games/donotdriveforwards/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1575149034,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/simpletravelcartop_view-sheet0.png",
+		"images/background2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
