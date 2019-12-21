fa33d99a4d60351ad89a124347557baf8e5557ac
tininsteelian <tininsteelian@gmail.com>
2019-12-21 16:30:22 -0500

    Cactus Fork
    Cactus Fork

---
+++ games/cactusfork/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1576963633,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/bacon-sheet0.png",
+		"media/crash.m4a",
+		"media/crash.ogg",
+		"media/instrument.m4a",
+		"media/instrument.ogg",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
