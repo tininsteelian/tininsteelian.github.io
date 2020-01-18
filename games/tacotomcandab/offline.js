ca4038d47816c8112c944ac756dcf3cf5d5a9867
tininsteelian <tininsteelian@gmail.com>
2020-01-18 16:54:08 -0500

    yes he can
    dab, that is

---
+++ games/tacotomcandab/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1579384287,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ttcd-sheet0.png",
+		"images/ttcd-sheet1.png",
+		"images/notel-sheet0.png",
+		"images/noter-sheet0.png",
+		"media/oops.m4a",
+		"media/oops.ogg",
+		"media/maple.m4a",
+		"media/maple.ogg",
+		"media/maple8bit.m4a",
+		"media/maple8bit.ogg",
+		"media/maplepiano.m4a",
+		"media/maplepiano.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
