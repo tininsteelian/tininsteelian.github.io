834043dae16e9ffe87169f2304bcceaf18b9ecc2
tininsteelian <tininsteelian@gmail.com>
2020-01-25 16:23:12 -0500

    i hate what i have created

---
+++ games/shoeshoo/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1579987202,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/shoe-sheet0.png",
+		"images/foot-sheet0.png",
+		"images/pexelsphoto-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/end.m4a",
+		"media/end.ogg",
+		"media/newshoe.m4a",
+		"media/newshoe.ogg",
+		"media/step.m4a",
+		"media/step.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
