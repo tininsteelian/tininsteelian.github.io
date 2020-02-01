6a1dbcc557f211993033a8a10f08c24281b30701
tininsteelian <tininsteelian@gmail.com>
2020-02-01 16:29:55 -0500

    pearpairs

---
+++ games/pearpairs/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1580592290,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pear-sheet0.png",
+		"images/pear-sheet1.png",
+		"images/pear2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/connect.m4a",
+		"media/connect.ogg",
+		"media/grab2.m4a",
+		"media/grab2.ogg",
+		"media/pearpolcaypse.m4a",
+		"media/pearpolcaypse.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"heebo-bold-webfont.woff",
+		"heebo-bold-webfont.woff2",
+		"heebo-regular-webfont.woff",
+		"heebo-regular-webfont.woff2",
+		"stylesheet.css"
+	]
+}
