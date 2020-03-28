6db48ac36d1c9655297cf930e17c062dff58ee12
tininsteelian <tininsteelian@gmail.com>
2020-03-28 17:09:27 -0400

    fuzzy dice

---
+++ games/fuzzydice/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1585429646,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"media/clack.m4a",
+		"media/clack.ogg",
+		"media/clack2.m4a",
+		"media/clack2.ogg",
+		"media/select.m4a",
+		"media/select.ogg",
+		"media/clack3.m4a",
+		"media/clack3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
