498bc7427c95752dcbf6d2ec4f48b222012f855e
tininsteelian <tininsteelian@gmail.com>
2020-04-04 17:20:32 -0400

    guess
    do it

---
+++ games/howmanyballoons/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1586034844,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/balloon-sheet0.png",
+		"images/balloon-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"penumbra.png"
+	]
+}
