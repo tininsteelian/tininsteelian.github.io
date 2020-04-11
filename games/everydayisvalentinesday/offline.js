0f5cb323bb59a47cd78fb30cf32933b68b8ae0da
tininsteelian <tininsteelian@gmail.com>
2020-04-11 17:28:55 -0400

    this game blows

---
+++ games/everydayisvalentinesday/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1586640344,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/heart-sheet0.png",
+		"images/heart-sheet1.png",
+		"images/heart-sheet2.png",
+		"images/sprite2-sheet0.png",
+		"media/fan.m4a",
+		"media/fan.ogg",
+		"media/heart.m4a",
+		"media/heart.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
