a068d63089888f3834744e2831a67e1ad95a0cc9
tininsteelian <tininsteelian@gmail.com>
2020-05-09 17:48:14 -0400

    are mushrooms wheels?
    yes

---
+++ games/aremushroomswheels/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1589060724,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/car-sheet0.png",
+		"images/mushroom-sheet0.png",
+		"images/road-sheet0.png",
+		"images/background-sheet0.png",
+		"media/242740__marlonhj__engine.m4a",
+		"media/242740__marlonhj__engine.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
