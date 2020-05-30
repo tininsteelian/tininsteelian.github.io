fc3f4c9fb8d9e36bb49a86844eecf286183a7180
tininsteelian <tininsteelian@gmail.com>
2020-05-30 17:16:41 -0400

    not imperfect
    definitely a bowl

---
+++ games/perfectpopcornbowl/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1590873221,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bowl-sheet0.png",
+		"images/kernel-sheet0.png",
+		"images/kernel-sheet1.png",
+		"images/wave-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sky828648_-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"media/pop04.m4a",
+		"media/pop04.ogg",
+		"media/pop1.m4a",
+		"media/pop1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
