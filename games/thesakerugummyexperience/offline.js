e204f266c1071f0f538eb405bd92c684a2e68cb2
tininsteelian <tininsteelian@gmail.com>
2020-07-11 17:32:57 -0400

    the sakeru gummy experience
    LONG LONG MAAAAAAAAAAN

---
+++ games/thesakerugummyexperience/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1594502896,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tailpiece-sheet0.png",
+		"images/snake-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/sakeru-gum-long-long-man-ringtone.m4a",
+		"media/sakeru-gum-long-long-man-ringtone.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
