ed14f4cdfcc8b73797afb06b149da5451a342197
tininsteelian <tininsteelian@gmail.com>
2020-09-12 17:31:44 -0400

    numberflop
    it's not numberwang

---
+++ games/numberflop/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1599946076,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/blocks-sheet0.png",
+		"images/blocks-sheet1.png",
+		"images/sprite-sheet0.png",
+		"media/click.m4a",
+		"media/click.ogg",
+		"media/lostset.m4a",
+		"media/lostset.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
