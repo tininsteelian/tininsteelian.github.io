aa25b1c1ffd04f8c7b1ce246ab286210c4addad2
tininsteelian <tininsteelian@gmail.com>
2020-09-19 17:14:18 -0400

    tree
    tree

---
+++ games/nowthatsxtreeme/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1600549742,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/badzone-sheet0.png",
+		"images/stop-sheet0.png",
+		"images/background-sheet0.png",
+		"media/flip.m4a",
+		"media/flip.ogg",
+		"media/ouch.m4a",
+		"media/ouch.ogg",
+		"media/land.m4a",
+		"media/land.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
