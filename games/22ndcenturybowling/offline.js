87de57351d051b27abbdc834de5feea716d6ef78
tininsteelian <tininsteelian@gmail.com>
2020-10-17 18:12:08 -0400

    22nd century bowling and other stuff

---
+++ games/22ndcenturybowling/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1602972506,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"media/514385__sieuamthanh__bowling-strike.m4a",
+		"media/514385__sieuamthanh__bowling-strike.ogg",
+		"media/65244__carbilicon__bowling.m4a",
+		"media/65244__carbilicon__bowling.ogg",
+		"media/79358__improve__bowling-inne-edit.m4a",
+		"media/79358__improve__bowling-inne-edit.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
