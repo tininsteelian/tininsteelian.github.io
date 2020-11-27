af116e463594e3f576b9d4da8aaa45881a7163c3
tininsteelian <tininsteelian@gmail.com>
2020-11-27 16:05:51 -0500

    tin's old games and other games

---
+++ games/hoverboardpenguin/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1606086018,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platformnormal-sheet0.png",
+		"images/platformthinlr-sheet0.png",
+		"images/platformlargehole-sheet0.png",
+		"images/bumper-sheet0.png",
+		"images/platformsmall-sheet0.png",
+		"images/platformthinud-sheet0.png",
+		"images/goal-sheet0.png",
+		"images/walltop-sheet0.png",
+		"images/backgroundworld1-sheet0.png",
+		"media/tinhit1.m4a",
+		"media/tinhit1.ogg",
+		"media/bedtime_tune_full_mix_demo.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
