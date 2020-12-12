6658aed16a6e6d7cc08a288642db51bde48908c2
tininsteelian <tininsteelian@gmail.com>
2020-12-12 16:43:13 -0500

    nail darts

---
+++ games/naildarts/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1607809229,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/hammer-sheet0.png",
+		"images/nail-sheet0.png",
+		"images/archery-sheet0.png",
+		"images/background-sheet0.png",
+		"media/hammerhit.m4a",
+		"media/hammerhit.ogg",
+		"media/nailhit.m4a",
+		"media/nailhit.ogg",
+		"media/nailonnail.m4a",
+		"media/nailonnail.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
