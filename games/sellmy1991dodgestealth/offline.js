0d62d4d6493051f29138bf16ed76120fab677e2d
tininsteelian <tininsteelian@gmail.com>
2021-01-23 15:58:49 -0500

    Sell My 1991 Dodge Stealth

---
+++ games/sellmy1991dodgestealth/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1611435456,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/getdolla-sheet0.png",
+		"images/realtor156501_-sheet0.png",
+		"media/209578__zott820__cash-register-purchase.m4a",
+		"media/209578__zott820__cash-register-purchase.ogg",
+		"media/462362__breviceps__small-applause.m4a",
+		"media/462362__breviceps__small-applause.ogg",
+		"media/497042__5demayo__auction-caller.m4a",
+		"media/497042__5demayo__auction-caller.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
