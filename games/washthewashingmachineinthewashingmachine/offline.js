580d783f3037788b5deb403175c4c748a36628ed
tininsteelian <tininsteelian@gmail.com>
2021-11-07 02:16:11 -0500

    0h game jam

---
+++ games/washthewashingmachineinthewashingmachine/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1636269141,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/washingmachine-sheet0.png",
+		"images/inside-sheet0.png",
+		"images/foreground-sheet0.png",
+		"images/sponge-sheet0.png",
+		"media/tspt_door_bell_ding_dong_031.m4a",
+		"media/tspt_door_bell_ding_dong_031.ogg",
+		"media/scrub.m4a",
+		"media/scrub.ogg",
+		"media/washingmachine.m4a",
+		"media/washingmachine.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
