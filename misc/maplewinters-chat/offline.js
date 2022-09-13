294c33eded478a40c3adb69c20953c782f39ae74
tininsteelian <tininsteelian@gmail.com>
2022-09-12 22:54:05 -0400

    maplewinters-chat
    thing for maplewinters

---
+++ misc/maplewinters-chat/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1663037596,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/unknown-sheet0.png",
+		"images/speechbubblebottom-sheet0.png",
+		"images/9patch.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
